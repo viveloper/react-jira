807363d8747c486e7973c00e3623ca1d9b1fa8f4
viveloper <ucsik1234@gmail.com>
2021-07-27 00:26:44 +0900

    remove comment

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,11 +8,8 @@ export const store = configureStore({
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
